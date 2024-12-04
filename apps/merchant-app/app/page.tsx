36830cc69112fcddb0df9e7ac2e7779c0e47b208
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-12-05 01:25:56 +0530

    added workflow

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="text-2xl">
       From merchant
-      <Button appName="Button">Click me</Button>
+      <Button>Click me</Button>
     </div>
   );
 }
