16becdd186d7e05820c2c5108ddaf2ffa893da60
VishalChaudhary01 <vishalchaudhary8832@gmail.com>
2024-12-05 01:59:09 +0530

    test

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -3,7 +3,8 @@ import { Button } from "@repo/ui/button";
 export default function Home() {
   return (
     <div className="text-2xl font-semibold">
-      Welcome to merchant app
+      Welcome to merchant app,
+      <p>this is merchent app</p>
       <Button>Click me</Button>
     </div>
   );
